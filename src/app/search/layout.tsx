5b4a3d6f4b042027f760ba14ccd8e3ddc4633329
Mark Carrington <frumarkcarrington@gmail.com>
2023-11-02 17:58:01 +0530

    started the search page ui

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -8,12 +8,29 @@ export default function SearchLayout({
 }) {
   return (
     <Container>
-      <Header></Header>
-      <section>{children}</section>
+      <Header />
+      <main>
+        <section></section>
+        <aside></aside>
+      </main>
     </Container>
+  );
+}
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <div className="top-layer"></div>
+      <div className="bottom-layer"></div>
+    </HeaderContainer>
   );
 }
 
 const Container = styled.div``;
 
-const Header = styled.nav``;
+const HeaderContainer = styled.nav`
+  .top-layer {
+  }
+  .bottom-layer {
+  }
+`;
---
+++ src/app/search/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import styled from 'styled-components'
+
+export default function Page(){
+    return(
+        <Container>
+            
+        </Container>
+    )
+}
+
+const Container = styled.section``
