229374fe9aef05b2e6f0dd60a10d74556d722f3d
Mark Carrington <frumarkcarrington@gmail.com>
2023-11-10 13:34:55 +0530

    {

--- components/search components/SearchImageContainer.tsx
+++ components/search components/SearchImageContainer.tsx
@@ -1,44 +1,77 @@
 "use client";
 import styled from "styled-components";
-import { data } from "../../constants/types/appTypes";
 import { useEffect, useState } from "react";
 import { pullImageFromAxios } from "../../constants/data/fetchFromAPIS";
+import { get } from "http";
 
-const testSetData: data[] = [];
+function getFirstLetter(string: string) {
+  return string.charAt(0);
+}
+
+type testDataType = {
+  imageUrl: string;
+  username: string;
+  anchorLink: string;
+  profileLetter: string;
+};
+
+const testDataSet: testDataType[] = [
+  {
+    imageUrl: "/images/search-images/image-1.jpeg",
+    username: "Mark Carrington",
+    anchorLink: "https://www.google.com/",
+    profileLetter: getFirstLetter("Mark"),
+  },
+  {
+    imageUrl: "/images/search-images/image-2.jpeg",
+    username: "Mark Carrington",
+    anchorLink: "https://www.google.com/",
+    profileLetter: getFirstLetter("Mark"),
+  },
+  {
+    imageUrl: "/images/search-images/image-3.jpeg",
+    username: "Mark Carrington",
+    anchorLink: "https://www.google.com/",
+    profileLetter: getFirstLetter("Mark"),
+  },
+  {
+    imageUrl: "/images/search-images/image-4.jpeg",
+    username: "Mark Carrington",
+    anchorLink: "https://www.google.com/",
+    profileLetter: getFirstLetter("Mark"),
+  },
+  {
+    imageUrl: "/images/search-images/image-5.jpeg",
+    username: "Mark Carrington",
+    anchorLink: "https://www.google.com/",
+    profileLetter: getFirstLetter("Mark"),
+  },
+  {
+    imageUrl: "/images/search-images/image-6.jpeg",
+    username: "Mark Carrington",
+    anchorLink: "https://www.google.com/",
+    profileLetter: getFirstLetter("Mark"),
+  },
+];
+
+get;
 
 export default function SearchImageContainer() {
-  const [imageActiveState, setimageActiveState] = useState<data>();
-  useEffect(() => {
-    pullImageFromAxios();
+  const [imageActiveState, setimageActiveState] = useState<testDataType>({
+    imageUrl: "",
+    username: "",
+    anchorLink: "",
+    profileLetter: "",
   });
+
+  // useEffect(() => {
+  //   pullImageFromAxios();
+  // });
+
   return (
     <Container>
-      <div>
-        <h4>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas adipisci
-          quo doloremque. Id inventore mollitia cumque velit nesciunt voluptate
-          magni optio, soluta ad dolorem quo impedit esse delectus sunt illo?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi itaque
-          placeat eaque voluptate saepe omnis vero nisi libero nemo eveniet,
-          sequi ad minus voluptas praesentium beatae ut aperiam nam soluta! Modi
-          harum corporis nihil sed dolorem, quia, animi autem ex repudiandae
-          doloribus deserunt iusto optio provident vitae molestias fuga corrupti
-          exercitationem hic aspernatur perferendis nam necessitatibus
-          architecto! Itaque, aliquam animi. Eligendi quibusdam dolores
-          aspernatur dolorem, soluta cum dicta ratione incidunt aliquid
-          repudiandae reprehenderit laudantium possimus commodi ullam reiciendis
-          aperiam sit dolorum! Eum dolor officia dolore eaque exercitationem
-          nemo culpa ab. Molestiae consequatur dignissimos dolore recusandae
-          voluptates delectus illum suscipit, natus laboriosam doloribus numquam
-          dolorem nostrum dolor earum ratione amet! Alias praesentium quibusdam
-          quam eligendi nesciunt nobis saepe soluta vel quos. Commodi sequi,
-          nostrum quisquam cum incidunt neque fugit aut vel, laborum enim illo
-          quo soluta error voluptate eveniet eaque laudantium, molestias natus
-          rerum illum aliquid! Quibusdam itaque voluptatum facere? Magni!
-        </h4>
-      </div>
       <section className="inner-section">
-        {testSetData.map(() => (
+        {testDataSet.map(() => (
           <div className="image-item"></div>
         ))}
       </section>
